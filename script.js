a84de591067c542a1fcb0f2a9da99a9fc5bf5e82
Navdeshsinghsaini <navdeshsinghsaini@gmail.com>
2023-02-16 07:47:47 +0530

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+let button = document.querySelector( "#btn" );
+let guess = document.querySelector("#guess").value;
+let randomSpace = document.querySelector("#num");
+let res = document.querySelector("#respond");
+
+button.addEventListener('click' , randomNumber)
+
+ 
+function randomNumber() {
+    let x = Math.floor(Math.random () * 40 )   - 20 ;
+    randomSpace.textContent = x;
+     hotCold(x);
+
+}
+
+
+function hotCold(x) {
+    let diff = git statusNumber(guess) - Number(x);
+    if (diff <= 5){
+     res.textContent = "Cold";
+    }else{
+        res.textContent = "Hot";
+     }
+}
 
